49798118c11b75f8572419b533963cf70c38ed0d
MausPotato <goregoro@gmail.com>
2026-06-06 03:55:45 +0800

    Creat card component and render deck

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import Card from './components/Card.vue'
+
+  const cards = []
+  for (let i = 0; i < 52; i++) {
+    cards.push({
+      id: i + 1,
+      suit: Math.ceil((i + 1) / 13),
+      point: (i % 13) + 1,
+    })
+  }
+  function handleClick(card) {
+    console.log('點到這張牌:', card)
+  }
+</script>
+
+<template>
+  <Card 
+    v-for="card in cards" 
+    :key="card.id" 
+    :card="card" 
+    @card-click="handleClick" />
+</template>
+
+<style scoped></style>
---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<script setup>
+  const props = defineProps({
+    card: Object,
+    required: true
+  })
+  const emit = defineEmits(['card-click'])
+  function handleClick() {
+    emit('card-click', props.card)
+  }
+</script>
+<template>
+  <div 
+    class="card" 
+    :style="{ backgroundImage: `url(/card/card_${card.id}.png)`}"
+    @click="handleClick"
+  ></div>
+</template>
+<style>
+  .card {
+    width: var(--card-width, 8vw);
+    aspect-ratio: 16 / 21;
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+    /* position: absolute; */
+    top: 100%;
+    left: 50%;
+  }
+</style>
