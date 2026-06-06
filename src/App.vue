9c29fdda3664071c0f75a0f45d8ae171c30cd81b
MausPotato <goregoro@gmail.com>
2026-06-07 00:51:47 +0800

    Add basic free cell movement

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
+  import { ref, reactive } from 'vue'
   import GameBoard from './components/GameBoard.vue'
-  // import Card from './components/Card.vue'
 
+  const selectedSource = ref(null)
   const cards = []
   for (let i = 0; i < 52; i++) {
     cards.push({
@@ -10,26 +11,80 @@
       point: (i % 13) + 1,
     })
   }
-  const gameState = {
+  const gameState = reactive({
     freeCells: [null, null, null, null],
     foundations: [[], [], [], []],
     tableau: [[], [], [], [], [], [], [], []]
-  }
-  function handleClick({ card, columnIndex, cardIndex }) {
-    console.log('點到這張牌:', card, columnIndex, cardIndex)
-  }
-
-  function handleFreeCellClick({ card, cellIndex }) {
-    console.log('點到 free cell:', card, cellIndex)
-  }
-
-  function handleFoundationClick({ foundation, foundationIndex }) {
-    console.log('點到 foundation', foundation, foundationIndex)
-  }
-
+  })
   cards.forEach((card, index) => {
     gameState.tableau[index % 8].push(card)
   })
+
+  function handleClick({ card, columnIndex, cardIndex }) {
+    if (selectedSource.value?.area === 'freeCell') {
+      moveFreeCellToTableau(selectedSource.value, columnIndex)
+      return
+    }
+
+    selectedSource.value = {
+      area: 'tableau',
+      card,
+      columnIndex,
+      cardIndex
+    }
+    console.log('點到 tableau 牌:', selectedSource.value)
+  }
+
+  function moveFreeCellToTableau(source, targetCloumnIndex) {
+    const moveCard = gameState.freeCells[source.cellIndex]
+    if (!moveCard) return
+    if (!source) return
+    gameState.tableau[targetCloumnIndex].push(moveCard)
+    gameState.freeCells[source.cellIndex] = null
+    selectedSource.value = null
+    console.log('freecell 移到 tableau', moveCard, targetCloumnIndex)
+  }
+
+  function handleFreeCellClick({ card, cellIndex }) {
+    if (!selectedSource.value && card) {
+      selectedSource.value = {
+        area: 'freeCell',
+        card,
+        cellIndex
+      }
+      console.log('選到freeCell牌', selectedSource.value)
+      return
+    }
+    if (!selectedSource.value) return
+    if (card) {
+      console.log('這裡已經有牌了!')
+      return
+    }
+    
+    const source = selectedSource.value
+    // todo
+    if (source.area !== 'tableau') return
+    const sourceColumn = gameState.tableau[source.columnIndex]
+    const isLastCard = source.cardIndex === sourceColumn.length - 1
+    if (!isLastCard) {
+      console.log('只能移動一張!')
+      return
+    }
+    const moveCard = sourceColumn.pop()
+    gameState.freeCells[cellIndex] = moveCard
+    selectedSource.value = null
+    console.log('移到 free cell:', moveCard, cellIndex)
+  }
+
+
+  function handleFoundationClick({ foundation, foundationIndex }) {
+    console.log('點到 foundation', {
+      foundation,
+      foundationIndex,
+      selectedSource: selectedSource.value
+    })
+  }
+
 </script>
 
 <template>
